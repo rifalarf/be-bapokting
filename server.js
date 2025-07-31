3391f588b7cff2260066df652b2f360d05020887
rifalarf <rifalarifrahman23@gmail.com>
2025-07-31 21:19:03 +0700

    Remove conflicting express-fileupload middleware

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
